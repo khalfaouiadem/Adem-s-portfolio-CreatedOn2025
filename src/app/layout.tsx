ae56b2381952c6787b1a68cd6eefd281d58e6dbb
zidilyes <ilyeszid33@gmail.com>
2025-09-26 00:04:57 +0100

    Portfolio Website

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css';
+import Navbar from '../components/Navbar';
+import { ReactNode } from 'react';
+
+export const metadata = {
+  title: 'Adem Khalfaoui Portfolio',
+  description: 'Fullstack Developer Portfolio',
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="bg-gray-50 text-gray-900 font-sans">
+        {/* Navbar fixe */}
+        <Navbar />
+
+        {/* Contenu de la page */}
+        <main className="pt-20">{children}</main>
+      </body>
+    </html>
+  );
+}
